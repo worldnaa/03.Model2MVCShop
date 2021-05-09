f4ef53d447c38f4c4d45e48b15d0ea21401238a4
이유리 <@DESKTOP-AVFNNQE>
2021-05-10 04:01:58 +0900

    v2(21.05.09)

--- 03.Model2MVCShop(EL,JSTL)/src/main/java/com/model2/mvc/view/product/GetProductAction.java
+++ 03.Model2MVCShop(EL,JSTL)/src/main/java/com/model2/mvc/view/product/GetProductAction.java
@@ -1,5 +1,6 @@
 package com.model2.mvc.view.product;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -20,6 +21,26 @@ public class GetProductAction extends Action {//상품상세보기 요청
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		System.out.println("<<<<< GetProductAction : execute() 시작 >>>>>");
+		
+		String history = null;
+		Cookie[] cookies = request.getCookies();
+		if(cookies != null && cookies.length > 0) {
+			for(int i=0; i<cookies.length; i++) {
+				Cookie cookie = cookies[i];
+				if(cookie.getName().equals("history")) {
+					history = cookie.getValue();
+					history += "," + request.getParameter("prodNo");
+				}
+			}
+		}
+		
+		//쿠키 생성
+		Cookie cookie = new Cookie("history", history);
+		//음수값은 쿠키가 영구적으로 저장되지 않고, 웹브라우저가 종료될 때 삭제
+		cookie.setMaxAge(-1);
+		//response에 지정된 쿠키를 추가
+		response.addCookie(cookie);
+		System.out.println("저장된 쿠키 이름 : " + cookie.getName());
 		
 		int prodNo = Integer.parseInt(request.getParameter("prodNo"));
 		System.out.println("받은 prodNo : " + prodNo);
